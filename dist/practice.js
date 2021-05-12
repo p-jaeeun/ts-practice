6a8449636bbf3a3e9e6ddc553136262b38359707
p-jaeeun <j_brick@naver.com>
2021-05-12 13:27:34 +0900

    기본타입

--- dist/practice.js
+++ dist/practice.js
@@ -1,3 +1,14 @@
 "use strict";
+let count = 0;
+count += 1;
+// String
 const message = "hello world";
-console.log(message);
+// Boolean
+const done = false;
+// Array
+const numbers = [1, 2, 3];
+const messages = ["hello", "world"];
+let mightBeUndefined = undefined; // 문자열 또는 undefined인 경우
+let nullableNumber = null; // null 또는 숫자인 경우
+let color = "yellow";
+color = "orange";
--- src/practice.ts
+++ src/practice.ts
@@ -1,2 +1,20 @@
+let count = 0;
+
+count += 1;
+
+// String
 const message: string = "hello world";
-console.log(message);
+
+// Boolean
+const done: boolean = false;
+
+// Array
+const numbers: number[] = [1, 2, 3];
+const messages: string[] = ["hello", "world"];
+
+let mightBeUndefined: string | undefined = undefined; // 문자열 또는 undefined인 경우
+let nullableNumber: null | number = null; // null 또는 숫자인 경우
+
+let color: "red" | "orange" | "yellow" = "yellow"; // red,orange,yellow 중에서 하나만
+
+color = "orange";
